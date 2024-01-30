7d661c894688289c22bb0b491977241ca33b38d2
Mudit Ameta <zeusdeux@gmail.com>
2024-01-30 18:55:50 +0530

    [zdx_str] Start with a gap buffer implementation

--- zdx_str.h
+++ zdx_str.h
@@ -89,9 +89,47 @@ size_t sb_append_buf(sb_t sb[const static 1], const char *buf, const size_t buf_
 // same as sb_free(sb_t *const sb) but the [static 1] enforces a pointer non-null check during compile
 void sb_deinit(sb_t sb[const static 1]);
 
+// ---- GAP BUFFER ----
+
+#ifndef GB_REALLOC
+#define GB_REALLOC realloc
+#endif // GB_REALLOC
+
+#ifndef GB_FREE
+#define GB_FREE free
+#endif // GB_FREE
+
+#ifndef GB_ASSERT
+#define GB_ASSERT assertm
+#endif // GB_ASSERT
+
+#ifndef GB_INIT_LENGTH
+#define GB_INIT_LENGTH 1024 // sizeof(char) * 1024
+#endif // GB_INIT_LENGTH
+
+#ifndef GB_MIN_GAP_SIZE
+#define GB_MIN_GAP_SIZE 16 // sizeof(char) * 16
+#endif // GB_MIN_GAP_SIZE
+
+typedef struct gap_buffer {
+  char *buf;
+  size_t gap_start_;
+  size_t gap_end_;
+  size_t length;
+} gb_t;
+
+void gb_init(gb_t gb[const static 1]);
+void gb_deinit(gb_t gb[const static 1]);
+void gb_move_cursor(gb_t gb[const static 1], const uint64_t pos);
+void gb_insert_char(gb_t gb[const static 1], const char c);
+void gb_insert_cstr(gb_t gb[const static 1], const char cstr[static 1], const size_t cstr_len);
+void gb_delete_chars(gb_t gb[const static 1], const uint64_t count); // count +ve -> backspace, count -ve -> delete
+
 #endif // ZDX_STR_H_
 
 #ifdef ZDX_STR_IMPLEMENTATION
+
+// ---- STRING BUILDER IMPLEMENTATION ----
 
 static void sb_resize_(sb_t sb[const static 1], const size_t reqd_capacity)
 {
@@ -178,4 +216,155 @@ void sb_deinit(sb_t sb[const static 1])
   sb->length = 0;
   sb->capacity = 0;
 }
+
+
+// ---- GAP BUFFER IMPLEMENTATION ----
+
+#define gb_dbg(label, gb)                                               \
+  do {                                                                  \
+    const char *buf_as_cstr = gb_buf_as_cstr_(gb);                      \
+    dbg("%s buf %s \t| length %zu \t| gap start %zu \t| gap end %zu",   \
+        label, buf_as_cstr, gb->length, gb->gap_start_, gb->gap_end_);  \
+    GB_FREE((void *)buf_as_cstr);                                       \
+  } while(0)
+
+#define gb_len_with_gap(gb) (((gb)->length + ((gb)->gap_end_ - (gb)->gap_start_)) ? ((gb)->length + ((gb)->gap_end_ - (gb)->gap_start_) + 1) : 0)
+
+static const char *gb_buf_as_cstr_(const gb_t gb[const static 1])
+{
+  GB_ASSERT(gb != NULL, "[zdx str] Expected: valid gap buffer instance, Received: %p", (void *)gb);
+
+  // abc{____}cbde
+  // 012334566789a
+  // len = 7, start = 3, end = 6
+  // buf_len_with_gap = 7 + (6 - 3) + 1x
+  size_t buf_len_with_gap = gb_len_with_gap(gb);
+  if (buf_len_with_gap) {
+    buf_len_with_gap += 1;
+  }
+
+  /* dbg("buf_len_with_gap %zu", buf_len_with_gap); */
+
+  char *str = GB_REALLOC(NULL, (buf_len_with_gap + 1) * sizeof(char));
+  GB_ASSERT(str != NULL, "[zdx str] Allocation failed for buf as cstring");
+
+  for (size_t i = 0; i < buf_len_with_gap; i++) {
+    if (i >= gb->gap_start_ && i <= gb->gap_end_) {
+      str[i] = '.';
+    } else {
+      str[i] = gb->buf[i];
+    }
+  }
+
+  str[buf_len_with_gap] = '\0';
+
+  return str;
+}
+
+// Resize gb gap to GB_MIN_GAP_SIZE
+static void gb_resize_gap_(gb_t gb[const static 1], size_t gap_size)
+{
+  gb_dbg(">>", gb);
+  dbg(">> gap size %zu", gap_size);
+  GB_ASSERT(gb != NULL, "[zdx str] Expected: valid gap buffer instance, Received: %p", (void *)gb);
+  GB_ASSERT(gap_size >= 0, "[zdx str] Expected: non-negative gap size, Received: %zu", gap_size);
+
+  if (gap_size < GB_MIN_GAP_SIZE) {
+    gap_size = GB_MIN_GAP_SIZE;
+  }
+  // abc{}cbde
+  // resize gb->buf to include GB_MIN_GAP_SIZE
+  gb->buf = GB_REALLOC(gb->buf, gb_len_with_gap(gb) + gap_size);
+  dbg("++ resized to size %zu",  gb_len_with_gap(gb) + gap_size);
+  GB_ASSERT(gb->buf != NULL, "[zdx str] Allocation failed for resizing gb->buf to expand gap");
+
+  // Example 1:
+  // abc{}cbde....
+  // 0123345678
+  // len = 7
+  // dst = buf + gap_start
+  // src = buf + (gap_end + (len - gap_start) + 1)
+  // abc{....}cbde
+  // 012334566789a
+  //
+  // Example 2:
+  // abc{..}12345....
+  // 012334456789a
+  // len = 8
+  // dst = buf + gap_start
+  // src = buf + (gap_end + (len - gap_start) + 1)
+  // abc{......}12345
+  // 0123344567789abc
+  void *dst = (void *)(gb->buf + gb->gap_start_);
+  const void *src = (void *)(gb->buf + (gb->gap_end_ + (gb->length - gb->gap_start_) + 1));
+  memmove(dst, src, GB_MIN_GAP_SIZE);
+  gb->gap_end_ += (gap_size - 1);
+
+
+  gb_dbg("<<", gb);
+  // done
+  return;
+}
+
+void gb_init(gb_t gb[const static 1])
+{
+  gb_dbg(">>", gb);
+  GB_ASSERT(gb != NULL, "[zdx str] Expected: valid gap buffer instance, Received: %p", (void *)gb);
+
+  const size_t init_size = GB_INIT_LENGTH > GB_MIN_GAP_SIZE ? GB_INIT_LENGTH : GB_MIN_GAP_SIZE;
+
+  gb->gap_start_ = 0;
+  gb->gap_end_ = init_size - 1;
+  gb->length = 0;
+  gb->buf = GB_REALLOC(NULL, init_size * sizeof(char));
+  GB_ASSERT(gb->buf != NULL, "[zdx str] Allocation failed for initializing gb->buf to default capacity");
+
+  gb_dbg("<<", gb);
+  return;
+}
+
+void gb_deinit(gb_t gb[const static 1])
+{
+  gb_dbg(">>", gb);
+
+  GB_FREE(gb->buf);
+  gb->gap_start_ = 0;
+  gb->gap_end_ = 0;
+  gb->length = 0;
+  gb->buf = NULL;
+
+  gb_dbg("<<", gb);
+  return;
+}
+
+void gb_move_cursor(gb_t gb[const static 1], const uint64_t pos)
+{
+  (void) gb;
+  (void) pos;
+}
+
+void gb_insert_char(gb_t gb[const static 1], const char c)
+{
+  gb_dbg(">>", gb);
+  dbg(">> char %c", c);
+
+  GB_ASSERT(gb != NULL, "[zdx str] Expected: valid gap buffer instance, Received: %p", (void *)gb);
+
+  const size_t gap_size = gb->gap_end_ - gb->gap_start_;
+  GB_ASSERT(gap_size >= 0, "[zdx str] Expected: non-negative gap size, Received: %zu", gap_size);
+
+  if (!gap_size) {
+    gb_resize_gap_(gb, GB_MIN_GAP_SIZE);
+  }
+
+  gb->buf[gb->gap_start_++] = c;
+  gb->length++;
+
+  gb_dbg("<<", gb);
+  return;
+}
+
+void gb_insert_cstr(gb_t gb[const static 1], const char cstr[static 1], const size_t cstr_len);
+void gb_delete_chars(gb_t gb[const static 1], const uint64_t count); // count +ve -> backspace, count -ve -> delete
+
 #endif // ZDX_STR_IMPLEMENTATION
