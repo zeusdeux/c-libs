368c121c82bebbba2d9c105d98a68a2031a46d4b
Mudit Ameta <zeusdeux@gmail.com>
2024-02-18 19:25:48 +0100

    [zdx_simple_arena] Implement simpler error communication from within allocator

--- tests/zdx_simple_arena_test.c
+++ tests/zdx_simple_arena_test.c
@@ -1,39 +1,133 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <errno.h>
 #include "../zdx_util.h"
 #define ZDX_SIMPLE_ARENA_IMPLEMENTATION
 #include "../zdx_simple_arena.h"
 
+void test_arena_alloc(arena_t *const arena, const size_t sz, const size_t expected_offset, const size_t expected_alignment);
+
 int main(void)
 {
   size_t requested_arena_size = 4098;
-  arena_t arena = arena_create(requested_arena_size);
 
   /* arena_create */
   {
-    if (!arena.is_valid) {
-      log(L_ERROR, "Arena creation error: %s", strerror(errno));
-    }
-    assertm(arena.is_valid, "Expected: valid arena to be created, Received: false");
+    arena_t arena = arena_create(requested_arena_size);
+    assertm(!arena.err, "Expected: valid arena to be created, Received: %s -> %s", arena.err, strerror(errno));
     assertm(arena.arena != NULL, "Expected: non-NULL arena addr, Received: %p", arena.arena);
-    size_t expected_arena_size = round_up_to_page_size_(requested_arena_size);
+
+    size_t expected_arena_size = arena_round_up_to_page_size_(requested_arena_size);
     assertm(arena.size == expected_arena_size, "Expected: %ld, Received: %zu", expected_arena_size, arena.size);
     assertm(arena.offset == 0, "Expected: 0, Received: %zu", arena.offset);
+
+    assertm(arena_free(&arena) && !arena.err, "Expected: arena free to work, Received: %s -> %s", arena.err,  strerror(errno));
+
+    log(L_INFO, "[ARENA CREATE TESTS] OK!");
   }
 
   /* arena_free */
   {
-    if (!arena_free(&arena)) {
-      log(L_ERROR, "Arena free error: %s", strerror(errno));
-    }
-    assertm(!arena.is_valid, "Expected: arena to be invalidated on free, Received: true");
+    arena_t arena = arena_create(requested_arena_size);
+    assertm(!arena.err, "Expected: valid arena to be created, Received: %s -> %s", arena.err, strerror(errno));
+
+    assertm(arena_free(&arena) && !arena.err, "Expected: arena free to work, Received: %s -> %s", arena.err,  strerror(errno));
     assertm(arena.arena == NULL, "Expected: non-NULL arena addr, Received: %p", arena.arena);
     assertm(arena.size == 0, "Expected: 0, Received: %zu", arena.size);
     assertm(arena.offset == 0, "Expected: 0, Received: %zu", arena.offset);
+
+    log(L_INFO, "[ARENA FREE TESTS] OK!");
+  }
+
+  /* arena_alloc */
+  {
+    {
+      arena_t arena = arena_create(requested_arena_size);
+      assertm(!arena.err, "Expected: valid arena to be created, Received: %s -> %s", arena.err, strerror(errno));
+
+      /* 1 byte allocation */
+      test_arena_alloc(&arena, 1, 1, 1);
+      /* 2 bytes allocation */
+      test_arena_alloc(&arena, 2, 4, 2);
+      /* 3 bytes allocation */
+      test_arena_alloc(&arena, 3, 7, 4);
+      /* 5 bytes allocation */
+      test_arena_alloc(&arena, 5, 13, 8); // (3 from above get's padded to 8 to be divisible by alignment of 8) + 5 = 13
+      /* 4 bytes allocation */
+      test_arena_alloc(&arena, 4, 20, 4); // (13 from above get's padded to 16 to be divisible by alignment of 4) + 4 = 20
+      /* 11 bytes allocation */
+      test_arena_alloc(&arena, 11, 35, 8); // (20 from above get's padded to 24 to be divisible by alignment of 8) + 5 = 29
+
+      /* struct allocation */
+      typedef struct {
+        char a;
+        char b;
+      } two_chars;
+      two_chars *t = arena_alloc(&arena, sizeof(two_chars));
+      t->a = 'a';
+      t->b = 'b';
+      assertm(t->a == 'a', "Expected: 'a', Received: %c", t->a);
+      assertm(t->b == 'b', "Expected: 'b', Received: %c", t->b);
+
+      assertm(arena_free(&arena) && !arena.err,
+              "Expected: arena free to work, Received: %s -> %s", arena.err,  strerror(errno));
+
+      log(L_INFO, "[ARENA ALLOC HAPPY PATH TESTS] OK!");
+    }
+    {
+      arena_t arena = arena_create(requested_arena_size);
+      assertm(!arena.err, "Expected: valid arena to be created, Received: %s -> %s", arena.err, strerror(errno));
+
+      /* arena.size = -1; // this should trigger compiler error */
+      size_t arena_sz = arena.size;
+      arena.size = 0; // trigger error in arena_alloc
+      int *i = arena_alloc(&arena, 4);
+      (void) i;
+      assertm(arena.err, "Expected: Arena to show error: %s, Received: valid arena", arena.err);
+      /* reset arena to proper state */
+      arena.size = arena_sz;
+      arena.err = NULL;
+
+      assertm(arena_free(&arena) && !arena.err,
+              "Expected: arena free to work, Received: %s -> %s", arena.err,  strerror(errno));
+
+      log(L_INFO, "[ARENA ALLOC ERROR PATHS TESTS] OK!");
+    }
   }
 
   log(L_INFO, "<zdx_gap_buffer_test> All ok!\n");
   return 0;
+}
+
+void test_arena_alloc(arena_t *const arena, const size_t sz, const size_t expected_offset, const size_t expected_alignment)
+{
+  log(L_INFO, "[ARENA ALLOC TEST] size %zu, exp offset %zu, exp align %zu", sz, expected_offset, expected_alignment);
+  assertm(sz > 0, "Expected: test_arena_alloc called with size > 0, Received: %zu", sz);
+
+  int8_t *n_bytes = arena_alloc(arena, sz);
+  assertm(!arena->err, "Expected: arena_alloc to succeed, Received: %s", arena->err);
+  assertm(n_bytes != NULL, "Expected: a non-NULL allocation, Received: %p", (void *)n_bytes);
+  assertm(arena->offset == expected_offset, "Expected: %zu, Received: %zu", expected_offset, arena->offset);
+
+  uint64_t n_bytes_alignment = (uintptr_t)n_bytes % expected_alignment;
+  assertm(n_bytes_alignment == 0, "Expected: %p aligned to 4, Received: %llu",(void *)n_bytes, n_bytes_alignment);
+
+  if (sz < 2) {
+    char c = 'a';
+    n_bytes[0] = c;
+    assertm(n_bytes[0] == c, "Expected: %c, Received: %c", c, (char)n_bytes[0]);
+  }
+  if (sz < 3) {
+    n_bytes[1] = 20;
+    assertm(n_bytes[1] == 20, "Expected: 20, Received: %d", n_bytes[1]);
+  }
+  n_bytes[2] = -100;
+  assertm(n_bytes[2] == -100, "Expected: -100, Received: %d", n_bytes[2]);
+
+  /* the below should seg fault once once allocator protects padded memory correct */
+  /* n_bytes[sz] = 50; */
+  /* assertm(n_bytes[sz], "Nope"); // this should segfault */
 }
--- zdx_simple_arena.h
+++ zdx_simple_arena.h
@@ -49,22 +49,23 @@ void *arena_calloc(arena_t *const ar, const size_t count, const size_t sz); /* c
 #include <stddef.h>
 #include <stdint.h>
 
-#define SA_DEFAULT_PAGE_SIZE_IF_UNDEF 4096
 /* Nothing that allocates should be included in non-debug builds */
 #if defined(ZDX_TRACE_ENABLE) || defined(DEBUG)
 #include <stdio.h>
 #include "./zdx_util.h"
-#define ar_dbg(label, ar) dbg("%s arena %p \t| size %zu \t| offset %zu \t| valid %d \t", \
-                              (label), (ar)->arena, (ar)->size, (ar)->offset, (ar)->is_valid)
+#define ar_dbg(label, ar) dbg("%s next %p \t|arena %p \t| size %zu \t| offset %zu \t| err %s",  \
+                              (label),                                                          \
+                              (void *)((uintptr_t)(ar)->arena + (ar)->offset),                  \
+                              (ar)->arena, (ar)->size, (ar)->offset, (ar)->err)
 #else
 #define ar_dbg(...)
 #endif
 
 typedef struct Arena {
-  void *arena;
   size_t size;
   size_t offset;
-  bool is_valid;
+  void *arena;
+  const char *err;
 } arena_t;
 
 /* gg windows */
@@ -73,7 +74,30 @@ typedef struct Arena {
 #include <unistd.h>
 #include <sys/mman.h>
 
-static inline size_t round_up_to_page_size_(size_t sz)
+typedef enum {
+  ARENA_ENOMEM = 0,
+  ARENA_EINVAL,
+  ARENA_EACQFAIL,
+  ARENA_ERELFAIL
+
+} arena_err_code_t;
+
+
+static const char *arena_get_err_msg_(arena_err_code_t err_code)
+{
+  static const char *err_str[] = {
+    "Arena cannot allocate memory",
+    "Arena received invalid argument",
+    "Arena failed to acquire memory",
+    "Arena failed to release memory"
+  };
+
+  return err_str[err_code];
+}
+
+#define SA_DEFAULT_PAGE_SIZE_IF_UNDEF 4096
+
+static inline size_t arena_round_up_to_page_size_(size_t sz)
 {
   long page_size = sysconf(_SC_PAGESIZE);
   dbg(">> size %zu \t| system page size %ld", sz, page_size);
@@ -82,7 +106,9 @@ static inline size_t round_up_to_page_size_(size_t sz)
     dbg("<< system page size %ld \t| sysconf failed", page_size);
     return 0;
   }
-  page_size = page_size == 0 ? SA_DEFAULT_PAGE_SIZE_IF_UNDEF : page_size; /* this should never really happen but guarding just in case */
+  /* this should never really happen but guarding just in case */
+  page_size = page_size == 0 ? SA_DEFAULT_PAGE_SIZE_IF_UNDEF : page_size;
+
   size_t rounded_up_sz = sz < (size_t)page_size ? (size_t)page_size : ((sz / (size_t)page_size) + 1) * (size_t)page_size;
 
   dbg("<< rounded up size %zu", rounded_up_sz);
@@ -91,29 +117,31 @@ static inline size_t round_up_to_page_size_(size_t sz)
 
 arena_t arena_create(size_t sz)
 {
+  dbg(">> requested size %zu", sz);
+
   arena_t ar = {0};
 
-  if (!(sz = round_up_to_page_size_(sz))) {
-    dbg("<< rounded arena size %zu \t| rounding failed", sz);
-    ar.is_valid = false;
+  if (!(sz = arena_round_up_to_page_size_(sz))) {
+    ar.err = arena_get_err_msg_(ARENA_EINVAL);
     ar.arena = NULL;
+
+    dbg("<< rounded arena size %zu \t| rounding failed", sz);
     return ar;
   }
-
-  dbg(">> size %zu \t| sys page size %ld", sz, page_size);
 
   /* MAP_PRIVATE or MAP_SHARED must always be specified */
   /* from mmap man page - "Conforming applications must specify either MAP_PRIVATE or MAP_SHARED." */
   ar.arena = mmap(NULL, sz, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
   if (ar.arena == MAP_FAILED) {
-    ar.is_valid = false;
+    ar.err = arena_get_err_msg_(ARENA_EACQFAIL);
     ar.arena = NULL;
+
     ar_dbg("<<", &ar);
     return ar;
   }
 
-  ar.is_valid = true;
+  ar.err = NULL;
   ar.size = sz;
 
   ar_dbg("<<", &ar);
@@ -124,17 +152,114 @@ bool arena_free(arena_t *const ar)
 {
   ar_dbg(">>", ar);
 
-  if (ar->arena != NULL && munmap(ar->arena, ar->size) == 0) {
+  /* only unmap if arena has no pending errors and is non-NULL aka valid */
+  if (!ar->err && ar->arena != NULL && munmap(ar->arena, ar->size) == 0) {
+    ar->err = NULL;
     ar->arena = NULL;
     ar->size = 0;
     ar->offset = 0;
-    ar->is_valid = false;
-    dbg("<< arena freed? true");
+    ar_dbg("<<", ar);
     return true;
   }
 
-  dbg("<< arena freed? false");
+  /* retain any previous error if an errored arena was passed in */
+  if (!ar->err) {
+    ar->err = ar->arena == NULL ? arena_get_err_msg_(ARENA_EINVAL) : arena_get_err_msg_(ARENA_ERELFAIL);
+  }
+
+  ar_dbg("<<", ar);
   return false;
+}
+
+#define DEFAULT_ALIGNMENT sizeof(max_align_t)
+
+
+/*
+ * Unaligned memory accesses occur when you try to read N bytes of data
+ * starting from an address that is not evenly divisible by N (i.e. addr % N != 0).
+ * For example, reading 4 bytes of data from address 0x10004 is fine,
+ * but reading 4 bytes of data from address 0x10005 would be an unaligned memory access.
+ *
+ * Source: https://www.kernel.org/doc/html/v6.1/core-api/unaligned-memory-access.html#the-definition-of-an-unaligned-access
+ *
+ * Also, this is wasteful alignment as knowing proper alignment requires knowing the type
+ * so that we can use _Alignof() for example. But this comes from the quote below -
+ *
+ * Pointers returned by allocation functions such as malloc are suitably aligned for any object,
+ * which means they are aligned at least as strictly as max_align_t. max_align_t is usually
+ * synonymous with the largest scalar type, which is long double on most platforms, and
+ * its alignment requirement is either 8 or 16.
+ *
+ * Source: https://en.cppreference.com/w/c/types/max_align_t and https://stackoverflow.com/a/60133364
+ */
+static inline size_t arena_get_alignment_(size_t sz)
+{
+  /* this guard is so that we don't return an alignment value > DEFAULT_ALIGNMENT */
+  if (sz < DEFAULT_ALIGNMENT) {
+    /* the branches below return "natural alignment" for values belows DEFAULT_ALIGNMENT */
+    /* the code below obviously assumes DEFAULT_ALIGNMENT to be equal to 8 */
+    /* and that's fine as we are ok with wasted space if DEFAULT_ALIGNMENT > 8 */
+    if (sz <= 1) {
+      return sz;
+    }
+    if (sz <= 2) {
+      return 2;
+    }
+    if (sz <= 4) {
+      return 4;
+    }
+  }
+  return DEFAULT_ALIGNMENT;
+}
+
+/* aligned arena allocator */
+void *arena_alloc(arena_t *const ar, const size_t sz)
+{
+  ar_dbg(">>", ar);
+  dbg(">> requested size %zu", sz);
+
+  /* validity check */
+  if (ar->err || ar->arena == NULL || ar->size <= 0 || ar->offset > ar->size || ar->offset < 0) {
+    ar->err = ar->err != NULL ? ar->err : arena_get_err_msg_(ARENA_EINVAL);
+
+    ar_dbg("<<", ar);
+    return NULL;
+  }
+
+  uintptr_t ptr = (uintptr_t)ar->arena + ar->offset;
+
+  if (sz > 0) {
+    size_t alignment = arena_get_alignment_(sz);
+    size_t remainder = 0;
+
+    if ((remainder = ptr % alignment) != 0) {
+      size_t padding = alignment - remainder;
+      ptr += padding;
+    }
+    /* not using ptrdiff_t as ptr is guaranteed to be greater than ar->arena due to checks and ptr assignment above */
+    size_t ptr_offset = ptr - (uintptr_t)ar->arena;
+    size_t remaining = ar->size - ptr_offset;
+    dbg("<< padded ptr %p", (void *)ptr);
+
+    /* non-resizeable arena hence we return NULL to signal error */
+    if (sz > remaining) {
+      ar->err = arena_get_err_msg_(ARENA_ENOMEM);
+
+      dbg("<< remaining %zu", remaining);
+      ar_dbg("<<", ar);
+      return NULL;
+    }
+
+    /* we need to point to address after the one we are returning in ptr */
+    ar->offset = ptr_offset + sz;
+  }
+
+  /* clear errors if any as if we have reached here means we are all good */
+  ar->err = NULL;
+
+  ar_dbg("<<", ar);
+  dbg("<< ptr %p", (void *)ptr);
+  return (void *)ptr;
 }
 
 #elif defined(_WIN32) || defined(_WIN64)
