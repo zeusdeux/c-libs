e04a77c228c26c908049dade6847db8fb5a74e08
Mudit Ameta <zeusdeux@gmail.com>
2024-12-31 04:02:50 +0100

    [zdx_fast_hashtable] Remove dependency on assert.h and add license

--- zdx_fast_hashtable.h
+++ zdx_fast_hashtable.h
@@ -1,11 +1,38 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2024 Mudit Ameta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * DESCRIPTION
+ *
+ * A simple hashtable with max key length of 16 ASCII chars
+ * that must have a pre-defined max keys count when initialized.
+ *
+ */
 #ifndef ZDX_FAST_HASHTABLE_H_
 #define ZDX_FAST_HASHTABLE_H_
 
-#include <stdint.h>
-#include <assert.h>
-
 #if !defined(FHT_VALUE_TYPE)
-static_assert(0, "FHT_VALUE_TYPE must be defined to use zdx_fast_hashtable.h");
+_Static_assert(0, "FHT_VALUE_TYPE must be defined to use zdx_fast_hashtable.h");
 #endif
 
 #ifndef FHT_API
@@ -16,6 +43,8 @@ static_assert(0, "FHT_VALUE_TYPE must be defined to use zdx_fast_hashtable.h");
 #include "zdx_util.h"
 #define FHT_ASSERT assertm
 #endif
+
+#include <stdint.h>
 
 // TODO(mudit): Limit key length to max of 13 instead of 16?
 // We only support ASCII values as keys
